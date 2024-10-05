ecd7105ebcb783ebddae07bddf19476acb966812
Peter Gagliardi <ptrgags@gmail.com>
2024-10-05 13:04:09 +0000

    WIP on GA cayley table

--- ga_cayley/src/main.rs
+++ ga_cayley/src/main.rs
@@ -1,14 +1,16 @@
-use geometric_algebra::make_blades;
+use geometric_algebra::{make_blades, Signature};
 
-fn make_cayley_table(positive: u8, negative: u8, zero: u8) {
-    let signature = Signature::new(positive, negative, zero);
+fn make_cayley_table(positive: usize, negative: usize, zero: usize) -> Result<(), String> {
+    /* signature = Signature::new(positive, negative, zero)?;
     let blades = make_blades(signature);
 
     for i in 0..blades.len() {
         for j in 0..blades.len() {
-            product =
+            let product = blades[i] * blades[2];
         }
-    }
+    }*/
+
+    Err(String::from("Not implemented (yet)"))
 }
 
 
--- geometric_algebra/src/unit_blade.rs
+++ geometric_algebra/src/unit_blade.rs
@@ -1,16 +1,47 @@
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct UnitBlade {
-    blades: u8,
+    vectors: u8,
 }
 
 impl UnitBlade {
-    pub fn new(blades: u8) -> Self {
+    pub fn new(vectors: u8) -> Self {
         Self {
-            blades
+            vectors
         }
     }
+
+    pub fn pretty(&self, labels: &[&str]) -> String {
+        let components: Vec<&str> = (0..8).map(
+            |i| if self.vectors >> i & 1 == 1{
+                labels[i] 
+            } else {
+                ""
+            }  
+        ).collect();
+        components.join("")
+    }
+        
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn pretty_labels_vector() {
+        let x_hat = UnitBlade::new(0b1);
+
+        let result = x_hat.pretty(&["x", "y", "z"]);
+
+        assert_eq!(result, "x");
+    }
+
+    #[test]
+    fn pretty_labels_bivector() {
+        let x_hat = UnitBlade::new(0b101);
+
+        let result = x_hat.pretty(&["x", "y", "z"]);
+
+        assert_eq!(result, "xz");
+    }
 }
