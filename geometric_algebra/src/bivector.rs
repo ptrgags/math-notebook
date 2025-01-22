d4cf6f355f842e5e4e4b652b7e926277676932ca
Peter Gagliardi <ptrgags@gmail.com>
2025-01-22 21:27:33 +0000

    Finish typing out bivector multiplication tables

--- geometric_algebra/src/bivector.rs
+++ geometric_algebra/src/bivector.rs
@@ -118,7 +118,19 @@ impl Mul<Scalar> for Bivector {
     type Output = Bivector;
 
     fn mul(self, rhs: Scalar) -> Self::Output {
-        todo!()
+        let Scalar(r) = rhs;
+        Self {
+            xy: r * self.xy,
+            xz: r * self.xz,
+            xp: r * self.xp,
+            xn: r * self.xn,
+            yz: r * self.yz,
+            yp: r * self.yp,
+            yn: r * self.yn,
+            zp: r * self.zp,
+            zn: r * self.zn,
+            pn: r * self.pn,
+        }
     }
 }
 
@@ -126,7 +138,51 @@ impl Mul<Vector> for Bivector {
     type Output = (Vector, Trivector);
 
     fn mul(self, rhs: Vector) -> Self::Output {
-        todo!()
+        let Bivector {
+            xy,
+            xz,
+            xp,
+            xn,
+            yz,
+            yp,
+            yn,
+            zp,
+            zn,
+            pn,
+        } = self;
+        let Vector { x, y, z, p, n } = rhs;
+
+        // There are 10 * 5 = 50 terms in total
+
+        // 1-overlap part - One vector cancels and you have one vector
+        // remaining. This feels similar to a dot product, but beware! it
+        // _anticommutes_!
+        // This is 5 * 4 = 20 terms
+        let vec_part = Vector {
+            x: xy * y + xz * z + xp * p - xn * n,
+            y: -xy * x + yz * z + yp * p - yn * n,
+            z: -xz * x - yz * y + zp * p - zn * n,
+            p: -xp * x - yp * y - zp * z - pn * n,
+            n: -xn * x - yn * y - zn * z - pn * p,
+        };
+
+        // 0-overlap part - the blades wedge into a trivector
+        // This is 10 * 3 = 30 terms
+        // 30 + 20 = 50
+        let trivec_part = Trivector {
+            xyz: xy * z - xz * y + yz * x,
+            xyp: xy * p - xp * y + yp * x,
+            xyn: xy * n - xn * y + yn * x,
+            xzp: xz * p - xp * z + zp * x,
+            xzn: xz * n - xn * z + zn * x,
+            xpn: xp * n - xn * p + pn * x,
+            yzp: yz * p - yp * z + zp * y,
+            yzn: yz * n - yn * z + zn * y,
+            ypn: yp * n - yn * p + pn * y,
+            zpn: zp * n - zn * p + pn * z,
+        };
+
+        (vec_part, trivec_part)
     }
 }
 
@@ -134,7 +190,73 @@ impl Mul for Bivector {
     type Output = (Scalar, Bivector, Quadvector);
 
     fn mul(self, rhs: Self) -> Self::Output {
-        todo!()
+        let Self {
+            xy: axy,
+            xz: axz,
+            xp: axp,
+            xn: axn,
+            yz: ayz,
+            yp: ayp,
+            yn: ayn,
+            zp: azp,
+            zn: azn,
+            pn: apn,
+        } = self;
+        let Self {
+            xy: bxy,
+            xz: bxz,
+            xp: bxp,
+            xn: bxn,
+            yz: byz,
+            yp: byp,
+            yn: byn,
+            zp: bzp,
+            zn: bzn,
+            pn: bpn,
+        } = rhs;
+
+        // 10 x 10 = 100 terms
+
+        // 2-overlap part (scalar) - 1 x 10 terms = 10
+        let scalar_part = Scalar(
+            -axy * bxy
+                + -axz * bxz
+                + -axp * bxp
+                + axn * bxn
+                + -ayz * byz
+                + -ayp * byp
+                + ayn * byn
+                + -azp * bzp
+                + azn * bzn
+                + apn * bpn,
+        );
+        // 1-overlap part (bivector) - 10 x 6 terms = 60
+        // I'm noticing a pattern: each row has exactly 3 minus signs
+        let bivec_part = Bivector {
+            xy: -axz * byz - axp * byp + axn * bpn + ayz * bxz + ayp * bxp - ayn * bxn,
+            xz: axy * byz - axp * bzp + axn * bzn - ayz * bxy + azp * bxp - azn * bxn,
+            xp: axy * byp + axz * bzp + axn * bpn - ayp * bxy - azp * bxz - apn * bxn,
+            xn: axy * byn + axz * bzn + axp * bpn - ayn * bxy - azn * bxz - apn * bxp,
+            yz: -axy * bxz + axz * bxy - ayp * bzp + ayn * bzn + azp * byp - azn * byn,
+            yp: -axy * bxp + axp * bxy + ayz * bzp + ayn * bpn - azp * byz - apn * byn,
+            yn: -axy * bxn + axn * bxy + ayz * bzn + ayp * bpn - azn * byz - apn * byp,
+            zp: -axz * bxp + axp * bxz - ayz * byp + ayp * byz + azn * bpn - apn * bzn,
+            zn: -axz * bxn + axn * bxz - ayz * byn + ayn * byz + azp * bpn - apn * bzp,
+            pn: -axp * bxn + axn * bxp - ayp * byn + ayn * byp - azp * bzn + azn * bzp,
+        };
+
+        // 0-overlap part (quadvector) - 5 x 6 terms = 30
+        // The minus signs are due to swaps only, so there's two for each row and follow
+        // the same pattern
+        let quadvec_part = Quadvector {
+            xyzp: axy * bzp - axz * byp + axp * byz + ayz * bxp - ayp * bxz + azp * bxy,
+            xyzn: axy * bzn - axz * byn + axn * byz + ayz * bxn - ayn * bxz + azn * bxy,
+            xypn: axy * bpn - axp * byn + axn * byp + ayp * bxn - ayn * bxp + apn * bxy,
+            xzpn: axz * bpn - axp * bzn + axn * bzp + azp * bxn - azn * bxp + apn * bxz,
+            yzpn: ayz * bpn - ayp * bzn + ayn * bzp + azp * byn - azn * byp + apn * byz,
+        };
+
+        (scalar_part, bivec_part, quadvec_part)
     }
 }
 
