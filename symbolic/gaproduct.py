70d1cafbccf5a255ed78cb6c8fabb6ea44cabac0
Peter Gagliardi <ptrgags@gmail.com>
2025-03-02 21:15:47 +0000

    add more algebras

--- symbolic/gaproduct.py
+++ symbolic/gaproduct.py
@@ -26,11 +26,11 @@ BINARY_PRODUCT_FUNCS = {
         'func': lambda a, b: a & b,
     },
     'sandwich': {
-        'label': "Sandwich Product (ABA^{-1})",
+        'label': "Sandwich Product (A * B * A^{-1})",
         'func': lambda a, b: (a * b * a.inv()),
     },
     'unit_sandwich': {
-        'label': "Sandwich product (ABA.rev()))",
+        'label': "Sandwich product (A * B * A.rev()))",
         "func": lambda a, b: a >> b
     },
     'left_contraction': {
@@ -72,6 +72,24 @@ def display_binary_product(algebra, product):
 
 
 PRODUCT_CHOICES = BINARY_PRODUCT_FUNCS.keys()
+
+
+class PGA1D:
+    def __init__(self):
+        self.alg = kingdon.Algebra(1, 0, 1)
+        self.basis = list(self.alg.blades.values())
+
+    def make_even(self, label):
+        return self.alg.multivector(
+            e=f'{label}s',
+            e01=f'{label}ox',
+        )
+
+    def make_odd(self, label):
+        return self.alg.multivector(
+            e0=f'{label}o',
+            e1=f'{label}x',
+        )
 
 
 class PGA2D:
@@ -126,14 +144,118 @@ class PGA3D:
         )
 
 
+class CGA1D:
+    def __init__(self):
+        self.alg = kingdon.Algebra(2, 1)
+        self.basis = list(self.alg.blades.values())
+
+    def make_even(self, label):
+        return self.alg.multivector(
+            e=f'{label}s',
+            e12=f'{label}xp',
+            e13=f'{label}xn',
+            e23=f"{label}pn",
+        )
+
+    def make_odd(self, label):
+        return self.alg.multivector(
+            e1=f'{label}x',
+            e2=f'{label}p',
+            e3=f'{label}n',
+            e123=f"{label}xpn",
+        )
+
+
+class CGA2D:
+    def __init__(self):
+        self.alg = kingdon.Algebra(3, 1)
+        self.basis = list(self.alg.blades.values())
+
+    def make_even(self, label):
+        return self.alg.multivector(
+            e=f'{label}s',
+            e12=f'{label}xy',
+            e13=f'{label}xp',
+            e14=f'{label}xn',
+            e23=f"{label}yp",
+            e24=f"{label}yn",
+            e34=f"{label}pn",
+            e1234=f"{label}xypn"
+        )
+
+    def make_odd(self, label):
+        return self.alg.multivector(
+            e1=f'{label}x',
+            e2=f'{label}y',
+            e3=f'{label}p',
+            e4=f'{label}n',
+            e123=f"{label}xyp",
+            e124=f'{label}xyn',
+            e134=f'{label}xpn',
+            e234=f'{label}ypn'
+        )
+
+
+class CGA3D:
+    def __init__(self):
+        self.alg = kingdon.Algebra(4, 1)
+        self.basis = list(self.alg.blades.values())
+
+    def make_even(self, label):
+        return self.alg.multivector(
+            e=f'{label}s',
+            e12=f'{label}xy',
+            e13=f'{label}xz',
+            e14=f'{label}xp',
+            e15=f'{label}xn',
+            e23=f"{label}yz",
+            e24=f"{label}yp",
+            e25=f"{label}yn",
+            e34=f"{label}zp",
+            e35=f"{label}zn",
+            e45=f"{label}pn",
+            e1234=f"{label}xyzn",
+            e1235=f"{label}xyzn",
+            e1245=f"{label}xypn",
+            e1345=f"{label}xzpn",
+            e2345=f"{label}yzpn",
+        )
+
+    def make_odd(self, label):
+        return self.alg.multivector(
+            e1=f'{label}x',
+            e2=f'{label}y',
+            e3=f'{label}z',
+            e4=f'{label}p',
+            e5=f'{label}n',
+            e123=f'{label}xyz',
+            e124=f'{label}xyp',
+            e125=f'{label}xyn',
+            e134=f'{label}xzp',
+            e135=f'{label}xzn',
+            e145=f'{label}xpn',
+            e234=f'{label}yzp',
+            e235=f'{label}yzn',
+            e245=f'{label}ypn',
+            e345=f'{label}zpn',
+            e12345=f'{label}xyzpn'
+        )
+
+
 ALGEBRAS = {
+    'pga1': PGA1D(),
     'pga2': PGA2D(),
     'pga3': PGA3D(),
+    'cga1': CGA1D(),
+    'cga2': CGA2D(),
+    'cga3': CGA3D(),
 }
+
+ALGEBRA_CHOICES = ALGEBRAS.keys()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("algebra", choices=['pga2', 'pga3'])
+    parser.add_argument("algebra", choices=ALGEBRA_CHOICES)
     parser.add_argument("product", choices=PRODUCT_CHOICES)
     args = parser.parse_args()
 
