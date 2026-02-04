dd2bbb533d83e9afaacf8b7d48e9a83d2340fa26
Peter Gagliardi <ptrgags@gmail.com>
2026-02-04 12:43:37 +0000

    Start a preamble for CGA2D

---
+++ symbolic/preamble_cga2d.py
@@ -0,0 +1,11 @@
+import math
+import kingdon
+alg = kingdon.Algebra(3, 1, 0)
+
+
+def even(scalar, xy, xp, xm, yp, ym, pm, xypm):
+    return alg.multivector(e=scalar, e12=xy, e13=xp, e14=xm, e23=yp, e24=ym, e34=pm, e1234=xypm)
+
+
+def odd(x, y, p, m, xyp, xym, xpm, ypm):
+    return alg.multivector(e1=x, e2=y, e3=p, e4=m, e123=xyp, e124=xym, e134=xpm, e234=ypm)
